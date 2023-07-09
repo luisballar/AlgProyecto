6e3ead4a27a6e12525ca2255f855500d41cf34fd
luisballar <116319938+luisballar@users.noreply.github.com>
2023-07-09 11:37:55 -0600

    Add files via upload

---
+++ src/domain/Shotgun.java
@@ -0,0 +1,66 @@
+package domain;
+
+
+import services.ArchivoDAO;
+
+import java.io.IOException;
+import java.util.LinkedList;
+import java.util.Random;
+
+public class Shotgun {
+    private int totalChars;
+    private int fragmentos;
+    private int longitud;
+    private String caracteres;
+    private LinkedList<String> listaFragmentos = new LinkedList<String>();
+    ArchivoDAO archivoDAO = new ArchivoDAO();
+    String path = "text.txt";
+    String nuevoPath = "fragmentos.txt";
+
+
+    // cuenta los caracteres del archivo
+    public int totalCaracteres() throws IOException {
+        caracteres = archivoDAO.leerArchivo(path);
+
+        return caracteres.length();
+    }
+
+    // retorna random entre 0 y la cantidad de caracteres del archivo
+    private int caracteresRandom() throws IOException {
+        Random random = new Random();
+
+        int limite = totalCaracteres() - (longitud + 1);
+
+        return random.nextInt(limite); // 0 a contadorCaracteres - 1;
+    }
+
+    // random que oscila entre longitud-1 y longitud+1
+    public int randomLongitud(int longitud){
+        Random random = new Random();
+
+        int logitudPromedio = random.nextInt(3) + (longitud-1);
+
+        return logitudPromedio;
+    }
+
+    // fragmentador
+    public void fragmentador(int cantFragmentos, int longitud) throws IOException {
+        StringBuilder sb = new StringBuilder();
+
+        this.longitud = longitud;
+
+        for (int i = 0; i < cantFragmentos; i++) {
+            int inicioFragmento = caracteresRandom();
+            String nuevoFragmento = caracteres.substring(inicioFragmento, inicioFragmento + randomLongitud(longitud));
+
+            // agrega el fragmento a un StringBuilder
+            sb.append(nuevoFragmento).append("\n");
+            // agrega el fragmento a una lista
+            listaFragmentos.add(nuevoFragmento);
+        }
+
+        // agregar al archivo
+        archivoDAO.insertarTexto(nuevoPath, sb.toString());
+    }
+
+}
---
+++ src/services/ArchivoDAO.java
@@ -0,0 +1,64 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package services;
+
+import java.io.*;
+
+
+public class ArchivoDAO {
+
+    // crea el archivo en una ruta
+    public void generaArchivo(String path){
+        File archivo = new File(path);
+        try {
+            FileWriter imprimir = new FileWriter(archivo);
+        } catch (IOException e) {
+            throw new RuntimeException(e);
+        }
+    }
+
+    // escribe en el archivo
+    public void insertarTexto(String path, String textoGenerado){
+        File archivo = new File(path);
+
+        try {
+            FileWriter imprimir = new FileWriter(archivo, false);
+            imprimir.append(textoGenerado);
+            imprimir.close();
+            System.out.println("Se agregó el texto");
+        } catch (IOException e) {
+            throw new RuntimeException(e);
+        }
+    }
+
+    /*
+    //Método general para leer un archivo
+    public BufferedReader getReadFile(String nameFile) throws FileNotFoundException {
+        File file = new File(nameFile);
+        FileInputStream fis = new FileInputStream(file);
+        InputStreamReader isr = new InputStreamReader(fis);
+        BufferedReader br = new BufferedReader(isr);
+          return br;
+    }
+
+     */
+
+    public String leerArchivo(String path){
+        StringBuilder sb =  new StringBuilder();
+        File archivo = new File(path);
+
+        try (BufferedReader rd = new BufferedReader(new FileReader(archivo))){
+            String ln;
+            while((ln = rd.readLine()) != null){
+                sb.append(ln).append("\n");
+            }
+        } catch (IOException e) {
+            throw new RuntimeException(e);
+        }
+        return sb.toString();
+    }
+
+}
